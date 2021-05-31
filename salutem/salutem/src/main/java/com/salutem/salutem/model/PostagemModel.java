da329cbe33aec2d8cdf482981c6f78b08c311011
Gustavo <gustavo.thomaz.ismart@gmail.com>
2021-05-31 12:33:12 -0300

    Feat. Adicionando a tabela de Postagem

---
+++ salutem/salutem/src/main/java/com/salutem/salutem/model/PostagemModel.java
@@ -0,0 +1,89 @@
+package com.salutem.salutem.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_postagem")
+public class PostagemModel {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long idPostagem;
+	
+	@NotNull
+	@Size(min = 5, max = 45)
+	private String tituloPostagem;
+	
+	@NotNull
+	@Size (min = 5, max = 255)
+	private String descricaoPostagem;
+	
+	@Size(min = 5, max = 45)
+	private String urlImagemPostagem;
+	
+	@NotNull
+	@Size(min = 5, max = 45)
+	private String especialidadePostagem;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	public Long getIdPostagem() {
+		return idPostagem;
+	}
+
+	public void setIdPostagem(Long idPostagem) {
+		this.idPostagem = idPostagem;
+	}
+
+	public String getTituloPostagem() {
+		return tituloPostagem;
+	}
+
+	public void setTituloPostagem(String tituloPostagem) {
+		this.tituloPostagem = tituloPostagem;
+	}
+
+	public String getDescricaoPostagem() {
+		return descricaoPostagem;
+	}
+
+	public void setDescricaoPostagem(String descricaoPostagem) {
+		this.descricaoPostagem = descricaoPostagem;
+	}
+
+	public String getUrlImagemPostagem() {
+		return urlImagemPostagem;
+	}
+
+	public void setUrlImagemPostagem(String urlImagemPostagem) {
+		this.urlImagemPostagem = urlImagemPostagem;
+	}
+
+	public String getEspecialidadePostagem() {
+		return especialidadePostagem;
+	}
+
+	public void setEspecialidadePostagem(String especialidadePostagem) {
+		this.especialidadePostagem = especialidadePostagem;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+}
