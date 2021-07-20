df7dfcd6415bcdd1edb6e7d9f035e532db729d96
Gustavo <gustavo.thomaz.ismart@gmail.com>
2021-07-19 22:39:26 -0300

    inicio implementação like

---
+++ src/main/java/com/salutem/salutem/model/like.java
@@ -0,0 +1,11 @@
+package com.salutem.salutem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_like")
+public class like {
+	
+	private Long idLike;
+}
